6beae3ec602a808f06c2120de7345b139f400978
Diogo Menezes <diogo444@hotmail.com>
2020-09-20 16:33:38 +0100

    add helmet and compression, prepare deploy

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 module.exports = app;
---
+++ src/startup/prod.js
@@ -0,0 +1,10 @@
+const helmet = require('helmet');
+
+const compression = require('compression');
+
+module.exports = app => {
+  if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+    app.use(compression());
+  }
+};
