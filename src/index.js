1d072b2cadf23922d9d217100625014e56bf8715
Diogo Menezes <diogo444@hotmail.com>
2020-09-21 15:28:53 +0100

    added documentation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ require('./startup/config')();
 require('express-async-errors');
 const winston = require('winston');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/validation')();
 require('./startup/prod')(app);
+
+app.use(express.static('../documentation'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'documentation', 'index.html'));
+});
 
 module.exports = app;
